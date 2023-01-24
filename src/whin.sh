3e9d94a2c55724816cf3cd2f1fb7390dcc54d80f
lu4t <29571529+lu4t@users.noreply.github.com>
2023-01-24 23:53:10 +0100

    added create whatsapp group command

--- src/whin.sh
+++ src/whin.sh
@@ -1,25 +1,36 @@
 
 #!/bin/bash
-text=$1
-gid=$2
 
-if ! [[ "$text" =~ ^[a-zA-Z0-9\ ]*$ ]]; then
-    echo "Error: text parameter must be a string."
-    exit 1
-fi
+API_KEY="YOU_API_KEY_GOES_HERE"
 
-if [ -n "$gid" ]; then
-    if ! [[ $gid =~ ^[0-9]+$ ]]; then
-        echo "Error: gid parameter must be a number."
+if [ "$1" = "whin-create-group" ]; then
+    curl --http1.1 --request GET \
+    --url https://whin2.p.rapidapi.com/creategroup \
+    --header "X-RapidAPI-Host: whin2.p.rapidapi.com" \
+    --header "X-RapidAPI-Key: $API_KEY"
+    echo "check WhatsApp to see the new group whin has created for you"
+else
+    text=$1
+    gid=$2
+
+    if ! [[ "$text" =~ ^[a-zA-Z0-9\ ]*$ ]]; then
+        echo "Error: text parameter must be a string."
         exit 1
     fi
-    endpoint="https://whin2.p.rapidapi.com/send2group?gid=$gid"
-else
-    endpoint="https://whin2.p.rapidapi.com/send"
-fi
 
-curl --http1.1 --request POST --url $endpoint \
---header 'X-RapidAPI-Host: whin2.p.rapidapi.com' \
---header 'X-RapidAPI-Key: YOUR_API_KEY' \
---header 'content-type: application/json' \
---data "{\"text\":\"$text\"}"
+    if [ -n "$gid" ]; then
+        if ! [[ $gid =~ ^[0-9]+$ ]]; then
+            echo "Error: gid parameter must be a number."
+            exit 1
+        fi
+        endpoint="https://whin2.p.rapidapi.com/send2group?gid=$gid"
+    else
+        endpoint="https://whin2.p.rapidapi.com/send"
+    fi
+
+    curl --http1.1 --request POST --url $endpoint \
+    --header "X-RapidAPI-Host: whin2.p.rapidapi.com" \
+    --header "X-RapidAPI-Key: $API_KEY" \
+    --header 'content-type: application/json' \
+    --data "{\"text\":\"$text\"}"
+fi
